84d22a6ff8268e3d30de07d9504260273fbef4b2
vitassuper <vitassuper99@gmail.com>
2019-11-05 19:24:27 +0200

    Added HelloController

---
+++ CRUD/src/main/java/edu/javavt17/config/AppInit.java
@@ -0,0 +1,29 @@
+package edu.javavt17.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
